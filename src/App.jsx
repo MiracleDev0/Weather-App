78edd82d7a07873e50dd9037f40d97f183981f50
Esan Miracle <miracleesan7@gmail.com>
2025-01-15 00:32:57 +0100

    Light and dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <>
-      <div className={`${isLightMode? 'bg-gradient-to-r from-gray-200 to-gray-500': 'bg-gradient-to-r from-gray-500 to-gray-900 '} min-h-screen`}>
+      <div className={`${isLightMode? 'bg-gradient-to-r from-gray-200 to-gray-500': 'bg-gradient-to-r from-gray-700 to-gray-950 '} min-h-screen`}>
         <div className="flex justify-between items-center p-8">
         <div>
           <label className="relative inline-flex items-center cursor-pointer">
@@ -21,8 +21,8 @@ function App() {
               checked={isLightMode}
               onChange={toggleLightMode} 
             />
-            <div className="w-11 h-6 bg-gray-400 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-            <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+            <div className="w-11 h-6 bg-gray-400 peer-focus:outline-none peer-focus:ring-4 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600"></div>
+            <span className={`ml-3 text-sm font-medium ${isLightMode ? 'text-gray-800' : 'text-gray-300'}`}>
               {isLightMode ? 'Light Mode' : 'Dark Mode'}
             </span>
           </label>
@@ -45,12 +45,12 @@ function App() {
           <div className="grid grid-cols-[2fr_3fr] gap-8"> {/* Grid layout */}
 
             {/* Location and Time */}
-            <div className="bg-gray-200 p-6 rounded-lg shadow-lg flex flex-col justify-center items-center "> 
+            <div className={`${isLightMode ? 'bg-gray-300' : 'bg-gray-700'} p-6 rounded-lg shadow-lg flex flex-col justify-center items-center`}> 
               {/* ... (Location, Time, Date) */}
             </div>
 
             {/* Weather Info */}
-            <div className="bg-gray-200 p-6 rounded-lg shadow-lg flex flex-col justify-center items-center">
+            <div className={`${isLightMode ? 'bg-gray-300' : 'bg-gray-700'} p-6 rounded-lg shadow-lg flex flex-col justify-center items-center`}>
               {/* ... (Temperature, Feels like, Humidity, etc.) */}
             </div>
 
@@ -60,12 +60,12 @@ function App() {
           <div className="grid grid-cols-[1.8fr_3.2fr] gap-8 mt-8"> {/* Grid for forecasts */}
 
             {/* 5-Day Forecast */}
-            <div className="bg-gray-200 p-6 rounded-lg shadow-lg">
+            <div className={`${isLightMode ? 'bg-gray-300' : 'bg-gray-700'} p-6 rounded-lg shadow-lg`}>
               {/* ... (Forecast information) */}
             </div>
 
             {/* Hourly Forecast */}
-            <div className="bg-gray-200 p-6 rounded-lg shadow-lg">
+            <div className={`${isLightMode ? 'bg-gray-300' : 'bg-gray-700'} p-6 rounded-lg shadow-lg`}>
               {/* ... (Forecast information) */}
             </div>
 
